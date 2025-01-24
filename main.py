2d4d1a9b982b8156060b8379afe7ba45ecff8069
Adrián Doña Mateo <drnaia100@gmail.com>
2025-01-24 12:12:30 +0000

    Add steps to main

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from farm import Farm
 from animals.dog import Dog
 from animals.cow import Cow
 from animals.chicken import Chicken
+from animals.goose import Goose
 from crops import *
 from equipment.equipment import Equipment
 from equipment.faulty_equipment import FaultyEquipment
+from equipment.rusty_equipment import RustyEquipment
 
 if __name__ == "__main__":
     farm = Farm()
@@ -13,18 +15,30 @@ if __name__ == "__main__":
     farm.add_animal(Dog("Buddy"))
     farm.add_animal(Cow("Bessie"))
     farm.add_animal(Chicken("Clucky"))
+    farm.add_animal(Goose("Silly"))
 
     # Add crops
     farm.add_crop(Corn())
     farm.add_crop(Wheat())
     farm.add_crop(Rice())
+    farm.add_crop(Barley())
 
     # Add equipment
     farm.add_equipment(Equipment("Tractor"))
     farm.add_equipment(Equipment("Plow"))
-    farm.add_equipment(FaultyEquipment("Old Harvester"))
+
+    old_harv = FaultyEquipment("Old Harvester")
+    farm.add_equipment(old_harv)
+
+    farm.add_equipment(RustyEquipment("Sickle"))
 
     # Run the farm and print animal noises
+    print()
+    print("=========")
+    print("FIRST RUN")
+    print("=========")
+    print()
+
     print(farm.run_farm())
     print()
 
@@ -39,4 +53,20 @@ if __name__ == "__main__":
     # Operate all equipment and handle exceptions
     print("Operating equipment:")
     print(farm.operate_all_equipment())
+
+    # Fix the Old Harvester
+
+    old_harv.fix()
+
+    print()
+    print("==========")
+    print("SECOND RUN")
+    print("==========")
+    print()
+
+    print("Operating equipment:")
+    print(farm.operate_all_equipment())
+
+
+
 
