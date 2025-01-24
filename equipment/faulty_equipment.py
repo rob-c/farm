74b2d3f9e7dd242c37fbcce2c0d5aa14a11874c6
Adrián Doña Mateo <drnaia100@gmail.com>
2025-01-24 12:12:21 +0000

    Add method to fix faulty equipment

--- equipment/faulty_equipment.py
+++ equipment/faulty_equipment.py
@@ -3,4 +3,7 @@ from .equipment import Equipment
 class FaultyEquipment(Equipment):
     def operate(self):
         raise RuntimeError(f"{self.name} has malfunctioned!")
+    
+    def fix(self):
+        self.__class__ = Equipment
 
